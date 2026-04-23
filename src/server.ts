dea90ae98a841d4b3953698a2c563bda42b3bd76
Aman Gupta <amangupta.95@outlook.com>
2026-04-23 13:22:50 +0530

    refactor: remove prisma.config.ts and update schema for MongoDB support

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import config from './config/config';
+import { prisma } from './config/prisma';
 
 const app = express();
 
 app.use(express.json());
 
 const startServer = async () => {
+  prisma.$connect().then(() => {
+    console.log('Connected to the database successfully.');
+  });
   app.listen(config.port, () => {
     console.log(
       `Server is running on port ${config.port} in ${config.nodeEnv} mode.`,
@@ -14,6 +18,7 @@ const startServer = async () => {
 };
 
 startServer().catch((error) => {
+  prisma.$disconnect();
   console.error('Failed to start server:', error);
   process.exit(1);
 });
